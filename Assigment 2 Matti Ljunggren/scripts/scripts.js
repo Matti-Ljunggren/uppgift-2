046032e71c70b9b8f14653e0125f22ffe04f5e3c
Matti-Ljunggren <ljunggrenmatti@gmail.com>
2024-12-03 22:33:38 +0100

    Add files via upload

---
+++ Assigment 2 Matti Ljunggren/scripts/scripts.js
@@ -0,0 +1,28 @@
+    let vip=true
+    let age=17
+
+if(age>=18) {
+        alert('Access granted');
+    } else {
+        alert('Access denied');
+ }  
+
+ if(vip==true){
+    console.log("Welcome in my best VIP customer :)");
+} else{
+    console.log();
+    
+}
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
