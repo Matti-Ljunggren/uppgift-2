c99abbc827c14b94046819bab6b779c16c4a9591
Matti-Ljunggren <ljunggrenmatti@gmail.com>
2024-12-04 18:22:21 +0100

    Assigment 1_5 finished

---
+++ Assigment 1_5 Matti Ljunggren/scripts/scripts.js
@@ -0,0 +1,29 @@
+    let vip = false
+    let age = 17
+    if(vip == true){
+        console.log("Welcome in my best VIP customer :)");
+    } else {
+        
+        if(age >=18) {
+        alert("Access granted");
+    } else {
+        alert("Access denied");}
+    }
+        
+
+
+ 
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
